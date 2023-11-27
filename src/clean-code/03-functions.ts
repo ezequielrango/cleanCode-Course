acf79f578322233a9d4d5a9ae6f0b3bdd17251ef
ezequielrango <ezequielrango29@gmail.com>
2023-11-26 23:32:44 -0300

    functions

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,81 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getAllMovies(movieId: string) {
+        console.log({ movieId });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getAllMovieActors(id: string) {
+        console.log({ id });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getUsuario(ActorId: string) {
+        console.log({ ActorId });
+    }
+
+    // Crear una película
+    function movie(title: string, description: string, rating: number, cast: string[]) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActorIfActorNotExists(fullName: string, birthdate: Date): boolean {
+
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if (fullName === 'fernando') return false;
+
+        console.log('Crear actor');
+        return true;
+
+    }
+
+
+
+    // función para obtener información de una película por Id
+    function getMovieById(movieId: number) {
+        console.log({ movieId });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getActorsByMovieId(movieId: number) {
+        console.log({ movieId });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorById(id: number) {
+        console.log({ id });
+    }
+
+    interface MovieProps {
+        cast: string[]
+        description: string,
+        rating: number,
+        title: string,
+    }
+
+    // Crear una película
+    function createMovie({ title, description, rating, cast }: MovieProps) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function newActor(fullName: string, birthdate: Date): boolean {
+
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if (fullName === 'fernando') return false;
+
+        console.log('Crear actor');
+        return true;
+
+    }
+
+
+    
+
+})();
