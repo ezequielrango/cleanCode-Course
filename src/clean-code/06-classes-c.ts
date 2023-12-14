b817b09cc49854adc134ef99723c819ef38239a7
ezequielrango <ezequielrango29@gmail.com>
2023-12-14 00:03:53 -0300

    apply unique responsability in User Class

---
+++ src/clean-code/06-classes-c.ts
@@ -0,0 +1,99 @@
+(() => {
+
+    //Aplicando el principio de responsabilidad única
+    
+    //Priorizar composición frente a la herencia
+    type Gender = 'M' | 'F';
+
+    interface PersonProps {
+        birthdate: Date;
+        gender: Gender;
+        name: string;
+    }
+
+    class Person {
+        public birthdate: Date;
+        public gender: Gender;
+        public name: string;
+
+        constructor({ name, gender, birthdate }: PersonProps) {
+            this.name = name;
+            this.gender = gender;
+            this.birthdate = birthdate;
+        }
+    }
+
+    interface UserProps {
+        birthdate  : Date,
+        email      : string,
+        gender     : Gender,
+        name       : string,
+        role       : string,
+    }
+
+
+    class User {
+
+        public email       : string;
+        public lastAccess  : Date;
+        public role        : string;
+
+        constructor({
+            email,
+            role,
+        }: UserProps ) {
+            this.lastAccess = new Date();
+            this.email = email;
+            this.role = role;
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+
+    interface UserSettingsProps {
+        birthdate: Date;
+        email: string;
+        gender: Gender;
+        lastOpenFolder: string;
+        name: string;
+        role: string;
+        workingDirectory: string;
+    }
+
+    class UserSettings extends User {
+
+        public workingDirectory : string;
+        public lastOpenFolder   : string;
+        constructor({
+            workingDirectory,
+            lastOpenFolder,
+            email,
+            role,
+            name,
+            gender,
+            birthdate,
+        }: UserSettingsProps ) {
+            super({email, role, name, gender, birthdate});
+            this.workingDirectory = workingDirectory;
+            this.lastOpenFolder = lastOpenFolder;
+        }
+    }
+
+
+    const userSettings = new UserSettings({
+        birthdate: new Date('1999-09-03'),
+        email: 'eze@google.com',
+        gender:'M',
+        lastOpenFolder:'/home',
+        name:'Ezequiel',
+        role:'Admin',
+        workingDirectory: '/usr/home',
+    });
+
+    console.log({ userSettings });
+
+
+})();
