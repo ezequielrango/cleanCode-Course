684b6028b7e863380282797c94e5657d37414206
ezequielrango <ezequielrango29@gmail.com>
2023-12-14 02:06:20 -0300

    homework

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import './clean-code/04-homework'
 import './clean-code/05-dry'
 import './clean-code/06-classes-a'
 import './clean-code/06-classes-b'
+import './clean-code//07-homework'
+
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
